b84e815baac51bf2588d6398a8e3572a30349929
Saurabh Suri <82346668+surim0n@users.noreply.github.com>
2026-08-20 08:00:18 -0400

    Fix mobility exercise image clarity (#5)

---
+++ mobility-images.js
@@ -0,0 +1,8 @@
+window.SURI_IMG=Object.assign(window.SURI_IMG||{}, {
+  'Cat–cow':'assets/mobility/cat-cow.webp',
+  'Open-book thoracic rotation':'assets/mobility/open-book.webp',
+  '90/90 hip switches':'assets/mobility/hip-switches.webp',
+  'Half-kneeling hip-flexor stretch':'assets/mobility/hip-flexor.webp',
+  'Adductor rock-backs':'assets/mobility/adductor-rockback.webp',
+  'Diagonal child’s-pose reach':'assets/mobility/childs-pose-reach.webp'
+});
